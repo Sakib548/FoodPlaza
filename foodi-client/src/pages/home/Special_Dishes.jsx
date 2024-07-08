201d6a2c43cf855045ae0da09fed450000d847e5
Sakib548 <mdsakibhassan548@gmail.com>
2024-07-08 11:16:56 +0600

    Finshed slider and standout dishes

--- foodi-client/src/pages/home/Special_Dishes.jsx
+++ foodi-client/src/pages/home/Special_Dishes.jsx
@@ -1,9 +1,35 @@
 import { useEffect, useRef, useState } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import Cards from "./Cards";
 
+const simpleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    >
+      NEXT
+    </div>
+  );
+};
+
+const simplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    >
+      BACK
+    </div>
+  );
+};
 const Special_Dishes = () => {
   const [recipes, setRecipes] = useState([]);
   const slider = useRef(null);
@@ -23,22 +49,45 @@ const Special_Dishes = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+
+    nextArrow: <simpleNextArrow />,
+    prevArrow: <simplePrevArrow />,
   };
   return (
-    <div className="section-container my-20">
+    <div className="section-container my-20 relative">
       <div className="text-left">
         <p className="subtitle">Special Dishes</p>
         <h2 className="title md:w-[520px]">Standout Dishes From Our Menu</h2>
       </div>
-      <Slider {...settings}>
+
+      {/* arrow btn */}
+      <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
+        <button
+          onClick={() => slider?.current?.slickPrev()}
+          className="btn p-2 rounded-full ml-5"
+        >
+          <FaAngleLeft className="w-8 h-8 p-1" />
+        </button>
+        <button
+          onClick={() => slider?.current?.slickNext()}
+          className="btn p-2 rounded-full ml-5 bg-green"
+        >
+          <FaAngleRight className="w-8 h-8 p-1" />
+        </button>
+      </div>
+      <Slider
+        ref={slider}
+        {...settings}
+        className="overflow-hidden mt-10 space-x-5"
+      >
         {recipes.map((item, i) => (
           <Cards key={i} item={item} />
         ))}
-        <div>
+        {/* <div>
           <h3>1</h3>
-        </div>
+        </div> */}
       </Slider>
     </div>
   );
