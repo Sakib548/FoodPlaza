780c9484b49a93162c6bbb6ffa55896b9ab8afb2
Sakib548 <mdsakibhassan548@gmail.com>
2024-07-29 18:47:04 +0600

    firebase

---
+++ foodi-client/src/contexts/AuthProvider.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  const authInfo = {};
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- foodi-client/src/main.jsx
+++ foodi-client/src/main.jsx
@@ -1,8 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/Router.jsx'
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./contexts/AuthProvider";
+import "./index.css";
+import router from "./router/Router.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <RouterProvider router={router} />,
+  </AuthProvider>
+);
