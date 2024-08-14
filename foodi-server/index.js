b42ad7e5816d9c304612a4d42d414dbd5955185a
Sakib548 <mdsakibhassan548@gmail.com>
2024-08-14 09:22:37 +0600

    configured cart routes

--- foodi-server/index.js
+++ foodi-server/index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@demo-foodi-cluster.1mgqa.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodi-cluster`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-cluster.1mgqa.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodi-cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,7 +28,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //database & collections
+
+    const menuCollections = client.db("demo-foodi-client").collection("menus");
+    const cartCollections = client
+      .db("demo-foodi-client")
+      .collection("cartItems");
+
+    //all menu items operations
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    //all carts operations
+
+    //posting cart to db
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollections.insertOne(cartItem);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
